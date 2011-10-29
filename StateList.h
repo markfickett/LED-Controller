ff3b31cccf5c3d67ddd3a40b31b56fd967198110
Mark Fickett <mark.fickett@gmail.com>
2011-10-29 15:11:24 -0400

    Add StateList (uncompiled). (Forgot header.)

---
+++ StateList.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include "Namespace.h"
+#include "Config.h"
+#include "Color.h"
+#include "Interval.h"
+#include "Pattern.h"
+
+LED_CONTROLLER_NAMESPACE_ENTER
+
+#define STATE_CHAR_PASSED	'p'
+#define STATE_CHAR_FAILED	'f'
+#define STATE_CHAR_BROKEN	'b'
+
+/**
+ * A Pattern which represents a list of states. Visually, it is a sequence of
+ * colors, drawing from a small set of colors (green, black, blinking) which
+ * represents a set of states (passing, failed, broken) for tests.
+ */
+class StateList : public Pattern {
+	public:
+		typedef enum {
+			PASSED,
+			FAILED,
+			BROKEN
+		} State;
+	private:
+		int numKnownStates;
+		bool blinkOn;
+		Interval brokenBlinkInterval;
+		bool statesChanged;
+		Color* stateColors[STRIP_LENGTH];
+
+		static Color colorPassed(0x00FF00);
+		static Color colorFailed(0xFF0000);
+		Color colorBroken;
+	public:
+		StateList();
+
+		void parseStates(const char* stateString);
+
+		bool update();
+		void apply(Color* stripColors);
+};
+
+LED_CONTROLLER_NAMESPACE_EXIT
+
