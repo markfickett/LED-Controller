b248358c90b610032338220009e331ef1fadd9f1
Mark Fickett <mark.fickett@gmail.com>
2012-02-07 23:20:50 -0500

    Re-arrange and document ToBytes.

--- Serialization.py
+++ Serialization.py
@@ -9,6 +9,21 @@ __all__ = [
 from Manifest import HALF_PRECISION
 from Manifest import math
 
+def ToBytes(colors):
+	"""
+	Convert the given list of Colors to a string (byte array), suitable
+	for sending to the Arduino. This uses Color.getRgbBytes, and then packs
+	those bytes into a string either directly or (if HALF_PRECISION is True)
+	alternately into the upper and lower 4 bits of each byte.
+
+	@return a string with color values packed into it
+	"""
+	if HALF_PRECISION:
+		return ToBytesHalf(colors)
+	else:
+		return ToBytesFull(colors)
+
+
 def ToBytesFull(colors):
 	return ''.join(
 		[''.join(
@@ -30,9 +45,4 @@ def ToBytesHalf(colors):
 				colorBytes[byteIndex] = channel/0x10
 				upper = True
 	return ''.join([chr(c) for c in colorBytes])
-
-if HALF_PRECISION:
-	ToBytes = ToBytesHalf
-else:
-	ToBytes = ToBytesFull
 
